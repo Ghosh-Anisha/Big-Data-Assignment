f54b44de8379bad5c7f0ed4dadf68fcf0c398fb8
Ghosh-Anisha <73275208+Ghosh-Anisha@users.noreply.github.com>
2021-10-18 18:40:10 +0530

    updated mapper

--- Assignment_2/task_1/mapper.py
+++ Assignment_2/task_1/mapper.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import sys
-
-with open('dataset_1percent.txt') as f:
-    lines = f.readlines()
-	
-for line in lines:
+   
+for line in sys.stdin:
     temp = line.split()
     print(temp[0],",",temp[1])
 
