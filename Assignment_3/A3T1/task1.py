c559780992cc668614641a70d9d26818351c328c
gayathrisunil <56356145+gayathrisunil@users.noreply.github.com>
2021-11-02 20:51:28 +0530

    working task 1(finally!!!)

--- Assignment_3/A3T1/task1.py
+++ Assignment_3/A3T1/task1.py
@@ -1,16 +1,29 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import DoubleType
+from pyspark.sql.functions import avg
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
 import sys
+
+sc =SparkContext()
+sqlContext = SQLContext(sc)
 
 spark = SparkSession.builder.appName("A3T1").getOrCreate()
 
 df = spark.read.option("header",True).csv(sys.argv[2])
-count=sys.argv[1]
+countr=sys.argv[1]
 
-rdd1=df.filter(df.Country.contains(count))
+rdd1=df.filter(df.Country.contains(countr))
+rdd1= rdd1.withColumn("AverageTemperature", df["AverageTemperature"].cast(DoubleType()))
 
-rdd2=rdd1.map(df.City,df.AverageTemperature)
+cityList=rdd1.groupBy("City").avg("AverageTemperature")
+#cityList.show()
+#cityList.sort(key= lambda x: x[0])
+rdd1= rdd1.orderBy(rdd1.City)
 
-rdd3=rdd1.groupBy('City').mean('AverageTemperature').collect()
-
-
+cond= [rdd1["AverageTemperature"] > cityList["avg(AverageTemperature)"], rdd1["City"] == cityList["City"] ]
+res= rdd1.join(cityList, cond , "inner").select(rdd1.City).groupBy("City").count().collect()
+res.sort()
+for row in res:
+	print (row[0],"\t",row[1])
 
